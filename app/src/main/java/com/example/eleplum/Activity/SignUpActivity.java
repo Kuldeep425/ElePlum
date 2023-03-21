e28a2db4e0c53d238bac61b0f05c23f95a170cd6
Vishesh Pratap Singh <visheshpratapsingh18@gmail.com>
2023-03-21 20:21:35 +0530

    added otp sending by firebase

--- app/src/main/java/com/example/eleplum/Activity/SignUpActivity.java
+++ app/src/main/java/com/example/eleplum/Activity/SignUpActivity.java
@@ -1,7 +1,10 @@
 package com.example.eleplum.Activity;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
 import android.text.InputType;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -9,14 +12,23 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.eleplum.Models.User;
 import com.example.eleplum.R;
 import com.google.firebase.FirebaseAppLifecycleListener;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.FirebaseTooManyRequestsException;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.concurrent.TimeUnit;
 
 public class SignUpActivity extends AppCompatActivity {
     CheckBox signUpEleBox;
@@ -25,14 +37,62 @@ public class SignUpActivity extends AppCompatActivity {
     Button signUpBtn;
     String name,phoneNumber,password;
     FirebaseAuth authInstance;
+    private PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
     FirebaseDatabase firebaseDatabase;
+    PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallback;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
         // calling method to initializing the components
+        authInstance = FirebaseAuth.getInstance();
+
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            @Override
+            public void onVerificationCompleted(@NonNull PhoneAuthCredential credential) {
+                // This callback will be invoked in two situations:
+                // 1 - Instant verification. In some cases the phone number can be instantly
+                //     verified without needing to send or enter a verification code.
+                // 2 - Auto-retrieval. On some devices Google Play services can automatically
+                //     detect the incoming verification SMS and perform verification without
+                //     user action.
+                Log.d(TAG, "onVerificationCompleted:" + credential);
+
+                signInWithPhoneAuthCredential(credential);
+            }
+
+            @Override
+            public void onVerificationFailed(@NonNull FirebaseException e) {
+                // This callback is invoked in an invalid request for verification is made,
+                // for instance if the the phone number format is not valid.
+                Log.w(TAG, "onVerificationFailed", e);
+
+                if (e instanceof FirebaseAuthInvalidCredentialsException) {
+                    // Invalid request
+                } else if (e instanceof FirebaseTooManyRequestsException) {
+                    // The SMS quota for the project has been exceeded
+                }
+
+                // Show a message and update the UI
+            }
+
+            @Override
+            public void onCodeSent(@NonNull String verificationId,
+                                   @NonNull PhoneAuthProvider.ForceResendingToken token) {
+                // The SMS verification code has been sent to the provided phone number, we
+                // now need to ask the user to enter the code and then construct a credential
+                // by combining the code with a verification ID.
+                Log.d(TAG, "onCodeSent:" + verificationId);
+
+                // Save verification ID and resending token so we can use them later
+//                mVerificationId = verificationId;
+//                mResendToken = token;
+            }
+        };
         initializeTheComponents();
+
 
 //        if(isShowPass){
 //            phoneNumberTxt.setInputType(InputType.TYPE_CLASS_NUMBER);
@@ -79,6 +139,9 @@ public class SignUpActivity extends AppCompatActivity {
 
     }
 
+    private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
+    }
+
     // method to verify the data provided by the user
 
     boolean isValidData(int isUser){
@@ -104,7 +167,14 @@ public class SignUpActivity extends AppCompatActivity {
         if(isValidData(1)){
             User user=new User(name,phoneNumber,password);
 
-
+            PhoneAuthOptions options =
+                    PhoneAuthOptions.newBuilder(authInstance)
+                            .setPhoneNumber("+91"+phoneNumber)       // Phone number to verify
+                            .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                            .setActivity(this)                 // Activity (for callback binding)
+                            .setCallbacks(mCallbacks)          // OnVerificationStateChangedCallbacks
+                            .build();
+            PhoneAuthProvider.verifyPhoneNumber(options);
         }
     }
 
