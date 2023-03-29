4e20757d8a998e940cdd27d60dda296a786bf6d9
Kuldeep <knishan40025@gmail.com>
2023-03-30 00:21:54 +0530

    added sending notification when user created task

--- app/src/main/java/com/example/eleplum/Activity/MainActivityUser.java
+++ app/src/main/java/com/example/eleplum/Activity/MainActivityUser.java
@@ -1,5 +1,8 @@
 package com.example.eleplum.Activity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
@@ -7,8 +10,11 @@ import android.view.animation.ScaleAnimation;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.example.eleplum.Fragments.ChatFragment;
 import com.example.eleplum.Fragments.HomeUserFragment;
@@ -30,6 +36,9 @@ public class MainActivityUser extends AppCompatActivity {
 
         // calling method to initialize the components
          initialiazeTheComponents();
+
+         // get notification permission
+        getNotificationPerm();
 
          // set home fragment by default
           getSupportFragmentManager().beginTransaction()
@@ -191,6 +200,25 @@ public class MainActivityUser extends AppCompatActivity {
 
 
 
+    }
+
+    // get notification permission
+    private void getNotificationPerm() {
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.TIRAMISU){
+            int notifPer= ActivityCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS);
+            if(notifPer!= PackageManager.PERMISSION_GRANTED){
+                String [] perm={Manifest.permission.POST_NOTIFICATIONS};
+                ActivityCompat.requestPermissions(this,perm,100);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==100){
+            Toast.makeText(this, "Permission granted", Toast.LENGTH_SHORT).show();
+        }
     }
 
     // method to initializing the widgets,components,layout etc.
---
+++ app/src/main/java/com/example/eleplum/Models/Notification.java
@@ -0,0 +1,12 @@
+package com.example.eleplum.Models;
+
+import java.util.List;
+
+public class Notification {
+    private String id;
+    private String topic;
+    private double latitude;
+    private double longitude;
+    private String userId;
+    private List<String>AcceptedUser;
+}
