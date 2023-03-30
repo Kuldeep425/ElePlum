b01122009dbfd02b60b380eb7e4aa498600dde71
Vishesh Pratap Singh <visheshpratapsingh18@gmail.com>
2023-03-30 17:41:13 +0530

    electrician home page

---
+++ app/src/main/java/com/example/eleplum/Fragments/EleNotificationFragment.java
@@ -0,0 +1,29 @@
+package com.example.eleplum.Fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.eleplum.R;
+
+
+public class EleNotificationFragment extends Fragment {
+
+
+
+    public EleNotificationFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_ele_notification, container, false);
+    }
+}
