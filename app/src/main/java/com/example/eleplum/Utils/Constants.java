2ee5da6b7b16af2c1f81701ca6aa50eb25d29de3
Kuldeep <knishan40025@gmail.com>
2023-05-05 02:20:30 +0530

    added constants class to keep all constants in single file

---
+++ app/src/main/java/com/example/eleplum/Utils/Constants.java
@@ -0,0 +1,14 @@
+package com.example.eleplum.Utils;
+
+public class Constants {
+
+    // for shared preference
+    public static  String KEY_PREFERENCE_NAME="ElePlumAppSharedPreference";
+    public static String KEY_IS_SIGNED_IN="isSignedIn";
+    public static String KEY_USER_ID="userId";
+    public static String KEY_ELE_ID="electricianId";
+    public static String KEY_IS_USER="isUser";
+
+
+
+}
