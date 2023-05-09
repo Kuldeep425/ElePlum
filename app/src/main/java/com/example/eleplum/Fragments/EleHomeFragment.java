2bdc6232ee2d8952f7f43c3e8f15cf935f7d0a2e
Kuldeep <knishan40025@gmail.com>
2023-05-09 12:43:10 +0530

    added electrician data on home screen

--- app/src/main/java/com/example/eleplum/Fragments/EleHomeFragment.java
+++ app/src/main/java/com/example/eleplum/Fragments/EleHomeFragment.java
@@ -9,21 +9,28 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.eleplum.R;
 import com.example.eleplum.Utils.Constants;
 import com.example.eleplum.Utils.PreferenceManager;
+import com.google.android.gms.maps.model.Circle;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.messaging.FirebaseMessaging;
+import com.squareup.picasso.Picasso;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 
 public class EleHomeFragment extends Fragment {
    private PreferenceManager preferenceManager;
-
+   View view;
+   CircleImageView electricianPic;
+   TextView electricianName;
 
     public EleHomeFragment() {
         // Required empty public constructor
@@ -35,7 +42,17 @@ public class EleHomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        // initialize view
+        view=inflater.inflate(R.layout.fragment_ele_home, container, false);
+
         preferenceManager=new PreferenceManager(getContext());
+
+        // initialization
+        initialization();
+
+        // method to set the electrician profile on the screen
+        setElectricianData();
 
         // for getting fcm token
         FirebaseMessaging.getInstance().getToken().addOnCompleteListener(new OnCompleteListener<String>() {
@@ -53,8 +70,21 @@ public class EleHomeFragment extends Fragment {
 
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ele_home, container, false);
+        return view;
     }
+
+    // method to set the electrician data on the screen
+    private void setElectricianData() {
+        Picasso.get().load(preferenceManager.getString(Constants.KEY_PROFILE_IMAGE_URL)).into(electricianPic);
+        electricianName.setText(preferenceManager.getString(Constants.KEY_NAME));
+    }
+
+    // method to initialize the element
+    private void initialization() {
+        electricianPic=view.findViewById(R.id.ele_pic_image);
+        electricianName=view.findViewById(R.id.ele_name_textview);
+    }
+
     // method to save electrician 's fcm token  in db
     private void saveFcmToDatabase(String token) {
         DatabaseReference db= FirebaseDatabase.getInstance().getReference("ElePlum");
